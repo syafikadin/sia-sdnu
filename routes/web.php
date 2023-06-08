a7cd91c0710f91e5d862d7bb24c3bc8ef7c41bbe
syafikadin <syafik.adin222@gmail.com>
2023-06-09 02:55:02 +0700

    landing page and login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        "title" => 'Home'
+    ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
