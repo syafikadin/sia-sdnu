815e1bc06a843dbc0ffbce6f7bca2b9cdaec3add
syafikadin <syafik.adin222@gmail.com>
2023-09-21 14:34:42 +0700

    Recover Navbar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,234 +1,81 @@
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="" class="brand-link">
-      <img src="/assets/dist/img/logo.png" alt="Logo" class="brand-image img-circle">
-      <span class="brand-text font-weight-light">Aplikasi E-Raport</span>
-    </a>
-  
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3 sidebar-sticky">
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+            <span>Administrator</span>
+        </h6>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin') ? 'active' : '' }}" aria-current="page" href="/admin">
+                <span data-feather="home" class="align-text-bottom"></span>
                 Dashboard
-              </p>
-            </a>
-          </li>
-  
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-user-friends"></i>
-              <p>
-                Data User
-              </p>
-            </a>
-          </li>
-  
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-bullhorn"></i>
-              <p>
-                Pengumuman
-              </p>
-            </a>
-          </li>
-  
-          <li class="nav-item has-treeview">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-server"></i>
-              <p>
-                Data Master
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview bg-secondary">
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-school nav-icon"></i>
-                  <p>Profil Sekolah</p>
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-user-tie nav-icon"></i>
-                  <p>Data Guru</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/guru*') ? 'active' : '' }}" aria-current="page" href="/admin/guru">
+                <span data-feather="user" class="align-text-bottom"></span>
+                Data Guru
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-calendar-week nav-icon"></i>
-                  <p>Data Tahun Pelajaran</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/siswa*') ? 'active' : '' }}" aria-current="page" href="/admin/siswa">
+                <span data-feather="users" class="align-text-bottom"></span>
+                Data Siswa
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-book nav-icon"></i>
-                  <p>Data Mata Pelajaran</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/kelas*') ? 'active' : '' }}" aria-current="page" href="/admin/kelas">
+                <span data-feather="users" class="align-text-bottom"></span>
+                Data Kelas
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-layer-group nav-icon"></i>
-                  <p>Data Kelas & Wali</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/mapel*') ? 'active' : '' }}" aria-current="page" href="/admin/mapel">
+                <span data-feather="user" class="align-text-bottom"></span>
+                Data Mata Pelajaran
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-users nav-icon"></i>
-                  <p>Data Siswa</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/pembelajaran*') ? 'active' : '' }}" aria-current="page" href="/admin/pembelajaran">
+                <span data-feather="user" class="align-text-bottom"></span>
+                Data Pembelajaran
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-book-open nav-icon"></i>
-                  <p>Data Pembelajaran</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/tapel*') ? 'active' : '' }}" aria-current="page" href="/admin/tapel">
+                <span data-feather="user" class="align-text-bottom"></span>
+                Data Tahun Pelajaran
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-book-reader nav-icon"></i>
-                  <p>Data Ekstrakulikuler</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/nilai*') ? 'active' : '' }}" aria-current="page" href="/admin/nilai">
+                <span data-feather="user" class="align-text-bottom"></span>
+                Nilai
                 </a>
-              </li>
-  
-            </ul>
-          </li>
-  
-          <!-- Kurikulum 2006 -->
-          <li class="nav-header">SETTING RAPORT KTSP 2006</li>
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-list-ol"></i>
-              <p>
-                Mapping Mapel
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-greater-than-equal"></i>
-              <p>
-                KKM Mapel
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-columns"></i>
-              <p>
-                Interval Predikat
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-calendar-week"></i>
-              <p>
-                Input Tanggal Raport
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-check-square"></i>
-              <p>
-                Validasi Data
-              </p>
-            </a>
-          </li>
-  
-          <li class="nav-header">HASIL RAPORT KTSP 2006</li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-list-ol"></i>
-              <p>
-                Hasil Penilaian
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview bg-secondary">
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-check-circle nav-icon"></i>
-                  <p>Status Penilaian</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-check-square nav-icon"></i>
-                  <p>Hasil Pengelolaan Nilai</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-clipboard-check nav-icon"></i>
-                  <p>Nilai Raport Semester</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-  
-  
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-calendar-check"></i>
-              <p>
-                Rekap Kehadiran Siswa
-              </p>
-            </a>
-          </li>
-  
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-table"></i>
-              <p>
-                Leger Nilai Siswa
-              </p>
-            </a>
-          </li>
-  
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-print"></i>
-              <p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/raport*') ? 'active' : '' }}" aria-current="page" href="/admin/raport">
+                <span data-feather="user" class="align-text-bottom"></span>
                 Cetak Raport
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview bg-secondary">
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-print nav-icon"></i>
-                  <p>Raport Tengah Semester</p>
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="fas fa-print nav-icon"></i>
-                  <p>Raport Semester</p>
+            </li>
+            {{-- <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/rapors*') ? 'active' : '' }}" aria-current="page" href="/admin/teachers">
+                <span data-feather="user" class="align-text-bottom"></span>
+                Rapor
                 </a>
-              </li>
-            </ul>
-          </li>
-          <!-- End Kurikulum 2006 -->
-  
-  
-          <li class="nav-item bg-danger mt-2">
-            <a href="" class="nav-link" onclick="return confirm('Apakah anda yakin ingin keluar ?')">
-              <i class="nav-icon fas fa-sign-out-alt"></i>
-              <p>
-                Keluar / Logout
-              </p>
-            </a>
-          </li>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/penilaians*') ? 'active' : '' }}" aria-current="page" href="/admin/teachers">
+                <span data-feather="user" class="align-text-bottom"></span>
+                Penilaian
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('admin/beritas*') ? 'active' : '' }}" aria-current="page" href="/admin/teachers">
+                <span data-feather="user" class="align-text-bottom"></span>
+                Berita Sekolah
+                </a>
+            </li> --}}
         </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
-  </aside>
+</nav>
