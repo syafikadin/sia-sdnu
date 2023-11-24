0dfae0b68b738231d4406972443bd5ed6655d60d
syafikadin <syafik.adin222@gmail.com>
2023-11-24 22:45:50 +0700

    Add new style

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,46 +1,42 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     {{-- Bootstrap Icon --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
     {{-- CSS intern --}}
-    
-    <link rel="stylesheet" href="/css/footer.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/carousel.css">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/footers/">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <link rel="stylesheet" href="/css/footer.css">
+    <link rel="stylesheet" href="/css/footer-style.css">
+    <link rel="stylesheet" href="/css/home.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/galery.css">
+
     <script src="assets/dist/js/adminlte.min.js"></script>
 
-    {{-- Trix CSS --}}
-    {{-- <link rel="stylesheet" type="text/css" href="/css/trix.css">
-    <script type="text/javascript" src="js/trix.js"></script> --}}
-    
-
     <title>Siakad SDNU | {{ $title }}</title>
-  </head>
-  <body>
+</head>
 
-    @include('partials.navbar')
+<body>
 
-    <div class="container mt-4">
-      @yield('container')
+    <div>
+        @yield('container')
     </div>
 
     <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
-  </body>
+</body>
+
 </html>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,53 +1,93 @@
-<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #286454">
+{{-- <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #286454">
     <div class="container">
-      <a class="navbar-brand" href="/">SIAKAD SD NU</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === 'Home') ? 'active' : '' }}" aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === 'About') ? 'active' : '' }}" href="/profile">Profil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === 'All Posts') ? 'active' : '' }}" href="/facilities">Fasilitas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === 'Post Categories') ? 'active' : '' }}" href="/berita">Berita</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === 'User') ? 'active' : '' }}" href="/gallery">Galeri</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === 'User') ? 'active' : '' }}" href="/contacts">Kontak</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item dropdown">
-          @if(Auth::guard('web')->user())
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Helo {{ Auth::user()->username }}
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="/admin"><i class="bi bi-layout-text-window-reverse"></i> My Dashboard</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <form action="/logout" method="post">
-                @csrf
-                <button type="submit" class="dropdown-item">
-                  <i class="bi bi-box-arrow-right"></i> Logout
-                </button>
-              </form>
+        <a class="navbar-brand" href="/">SIAKAD SD NU</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link {{ $title === 'Home' ? 'active' : '' }}" aria-current="page"
+                        href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $title === 'About' ? 'active' : '' }}" href="/profile">Profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $title === 'All Posts' ? 'active' : '' }}" href="/facilities">Fasilitas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $title === 'Post Categories' ? 'active' : '' }}" href="/berita">Berita</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $title === 'User' ? 'active' : '' }}" href="/gallery">Galeri</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $title === 'User' ? 'active' : '' }}" href="/contacts">Kontak</a>
+                </li>
             </ul>
-          @else
-            <li class="nav-item">
-              <a class="nav-link {{ ($title === 'Login') ? 'active' : '' }}" href="/login"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-            </li>
-          @endif
-          
-        </ul>
-      </div>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    @if (Auth::guard('web')->user())
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Helo {{ Auth::user()->username }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/admin"><i class="bi bi-layout-text-window-reverse"></i>
+                                    My Dashboard</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <form action="/logout" method="post">
+                                @csrf
+                                <button type="submit" class="dropdown-item">
+                                    <i class="bi bi-box-arrow-right"></i> Logout
+                                </button>
+                            </form>
+                        </ul>
+                    @else
+                <li class="nav-item">
+                    <a class="nav-link {{ $title === 'Login' ? 'active' : '' }}" href="/login"><i
+                            class="bi bi-box-arrow-in-right"></i> Login</a>
+                </li>
+                @endif
+
+            </ul>
+        </div>
     </div>
-  </nav>
+</nav> --}}
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand " href="#">Siakad SD-NU</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Fasilitas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Berita</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="/login" class="btn btn-success button-success px-3"><i
+                            class="bi bi-box-arrow-in-right mr-2"></i>
+                        Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
