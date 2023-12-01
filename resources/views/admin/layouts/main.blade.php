a8db5944e86fa3ac45b87b0d4ea769742fd352d0
syafikadin <syafik.adin222@gmail.com>
2023-12-01 11:56:27 +0700

    Table sorting on users

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -8,13 +8,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+    {{-- Toastr --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 
     {{-- Custom styles for bootstrap dashboard --}}
-    <link href="/css/admin/sidebar.css" rel="stylesheet" />
+    <link href="/css/sidebar-dashboard/sidebar-dashboard.css" rel="stylesheet" />
 
     {{-- Fontsome Icon --}}
     <link href="/css/plugins/fontawesome-free/css/all.min.css" rel="stylesheet" />
+
+    {{-- Table Custom --}}
+    <link rel="stylesheet" href="/css/table-custom.css" />
+    <!-- Data Table CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css" />
 
 </head>
 
@@ -38,7 +44,20 @@
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 
+    {{-- Font Awesome --}}
+    <script src="https://kit.fontawesome.com/a3c04fec21.js" crossorigin="anonymous"></script>
+
+    {{-- Toastr --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
+    {{-- Table Custom --}}
+    <!-- Data Table JS -->
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.1.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="/js/my-table.js"></script>
+
 </body>
 
 </html>
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -6,84 +6,34 @@
     </div>
 
     @if (session()->has('success'))
-      <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
     @endif
 
-    <!-- Button Trigger Modal -->
-    {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-tambah">
-        Tambah User
-    </button> --}}
-    
-    <!-- Modal Tambah -->
-    {{-- <div class="modal fade" id="modal-tambah" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-        <div class="modal-content">
-            <form method="post" action="/admin/user" enctype="multipart/form-data">
-                @csrf
-                <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Tambah User</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="mb-2">
-                        <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" name="username" required autofocus value="{{ old('username') }}">
-                        @error('username')
-                            <div class="invalid-feedback">
-                            {{ $message }}
-                            </div>   
-                        @enderror
-                    </div>
-        
-                    <div class="mb-2">
-                        <label for="semester" class="form-label">Semester</label><br>
-                        <label class="radio-inline mr-3">
-                            <input type="radio" @error('semester') is-invalid @enderror id="semester" name="semester" required value="1" @if (old('semester')=='1' ) checked @endif> Semester Ganjil
-                        </label>
-                        <label class="radio-inline mr-3">
-                            <input type="radio" @error('semester') is-invalid @enderror id="semester" name="semester" required value="2" @if (old('semester')=='2' ) checked @endif> Semester Genap
-                        </label>
-                        @error('semester')
-                            <div class="invalid-feedback">
-                            {{ $message }}
-                            </div>   
-                        @enderror
-                    </div>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                </div>
-            </form>
-        </div>
-        </div>
-    </div> --}}
-    {{-- End Modal Tambah --}}
-
-    <div class="table-responsive col-lg-10">        
-        <table class="table table-striped table-sm mt-3">
+    <table id="my-table" class="table table-striped" style="width: 100%">
         <thead>
             <tr>
-                <th scope="col">No</th>
-                <th scope="col">Username</th>
-                <th scope="col">Level</th>
-                <th scope="col">Action</th>
+                <th>No</th>
+                <th>Username</th>
+                <th>Level</th>
+            </tr>
             </tr>
         </thead>
         <tbody>
-            @foreach ($data_user as $user)    
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $user->username }}</td>
-                <td>{{ $user->role == 1 ? 'Admin' : ($user->role == 2 ? 'Guru' : 'Siswa') }}</td>
-            </tr>
+            @foreach ($data_user as $user)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $user->username }}</td>
+                    <td>{{ $user->role == 1 ? 'Admin' : ($user->role == 2 ? 'Guru' : 'Siswa') }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
-            {{-- Modal Edit --}}
-            {{-- <div class="modal fade" id="modal-edit-{{ $siswa->id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    {{-- Modal Edit --}}
+    {{-- <div class="modal fade" id="modal-edit-{{ $siswa->id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <form method="post" action="/admin/siswa/{{ $siswa->id }}" enctype="multipart/form-data">
@@ -114,49 +64,6 @@
                                     @enderror
                                 </div>
 
-                                <div class="mb-2">
-                                    <label for="kelas" class="form-label">Kelas</label>
-                                    <select name="kelas_id" class="form-select" aria-label="Default select example">
-                                        <option selected>--Pilih Kelas--</option>
-                                        @foreach ($data_kelas as $kelas)
-                                            <option value="{{ $kelas->id }}" required @if($kelas->id == $siswa->kelas->id) selected @endif >{{ $kelas->nama_kelas }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-            
-                                <div class="mb-2">
-                                    <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
-                                    <div>
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" id="jenis_kelamin" name="jenis_kelamin" value="0" @if ($siswa->jenis_kelamin == 0) checked @endif required>
-                                            <label class="form-check-label" for="jenis_kelamin">Laki-laki</label>
-                                        </div>
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" id="jenis_kelamin" name="jenis_kelamin" value="1" @if ($siswa->jenis_kelamin == 1) checked @endif required>
-                                            <label class="form-check-label" for="jenis_kelamin">Perempuan</label>
-                                        </div>
-                                    </div>
-                                </div>
-            
-                                <div class="mb-2">
-                                    <label for="tanggal_lahir" class="form-label">Tanggal Lahir</label>
-                                    <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir" value="{{ $siswa->tanggal_lahir }}">
-                                    @error('tanggal_lahir')
-                                        <div class="invalid-feedback">
-                                        {{ $message }}
-                                        </div>   
-                                    @enderror
-                                </div>
-            
-                                <div class="mb-2">
-                                    <label for="alamat" class="form-label">Alamat</label>
-                                    <input type="text" class="form-control @error('alamat') is-invalid @enderror" id="alamat" name="alamat" required autofocus value="{{ $siswa->alamat }}">
-                                    @error('alamat')
-                                        <div class="invalid-feedback">
-                                        {{ $message }}
-                                        </div>   
-                                    @enderror
-                                </div>
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -166,11 +73,5 @@
                     </div>
                 </div>
             </div> --}}
-            {{-- End Modal Edit --}}
-
-
-            @endforeach
-        </tbody>
-        </table>
-    </div>
+    {{-- End Modal Edit --}}
 @endsection
