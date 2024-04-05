302534867e225fd03bb8ba5fbb35ba1992f03d14
syafikadin <syafik.adin222@gmail.com>
2024-04-05 13:56:40 +0700

    delete image

--- resources/views/components/galery.blade.php
+++ resources/views/components/galery.blade.php
@@ -3,15 +3,9 @@
         <img src="/img/IMG_0474.jpg" alt="galery-image">
     </div>
     <div class="item-galery">
-        <img src="/img/IMG_2277.jpg" alt="galery-image">
-    </div>
-    <div class="item-galery">
         <img src="/img/IMG_2399.jpg" alt="galery-image">
     </div>
     <div class="item-galery">
         <img src="/img/IMG_2140.jpg" alt="galery-image">
-    </div>
-    <div class="item-galery">
-        <img src="/img/IMG_0594.jpg" alt="galery-image">
     </div>
 </div>
